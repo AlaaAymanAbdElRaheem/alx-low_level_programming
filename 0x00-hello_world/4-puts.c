469e194020f49d9d969c60c1eac7ac6288f80010
AlaaAymanAbdElRaheem <alaa251996@gmail.com>
2023-03-16 21:28:58 +0200

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,12 @@
-# include<stdio.h>
+#include<stdio.h>
+
+/*
+ *this is the main function
+ *return 0
+ */
 
 int main(void)
 {
   puts("\"Programming is like building a multilingual puzzle");
-    return 0;
+  return (0);
 }
