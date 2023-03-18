159024cf7fce36f2196d69680722cfbb5fd04907
AlaaAymanAbdElRaheem <alaa251996@gmail.com>
2023-03-18 04:18:35 +0200

    task 12

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+
+/**
+ * main - prints combinations of numbers
+ * Return: 0
+ */
+
+int main(void)
+{
+	int num;
+	int num2;
+
+	for (num = 0; num < 9; num++)
+	{
+		for (num2 = 0; num2 <= 9; num2++)
+		{
+			if (num < num2 || num == num2)
+			{
+				putchar(num + '0');
+				putchar(num2 + '0');
+				putchar(' ');
+
+				for (num = 0; num < 9; num++)
+				{
+					for (num2 = 1; num2 <= 9; num2++)
+					{
+						if (num < num2 || num == num2)
+						{
+							putchar(num + '0');
+							putchar(num2 + '0');
+							if (num + num2 != 18)
+							{
+								putchar(',');
+								putchar(' ');
+							}
+						}
+					}
+				}
+			}
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
+}
