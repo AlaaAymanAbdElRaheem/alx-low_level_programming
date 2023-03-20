b6084e74fcf72982eb4a37d32e4753769b33904c
AlaaAymanAbdElRaheem <alaa251996@gmail.com>
2023-03-21 01:36:33 +0200

    fixing task 12

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -12,18 +12,18 @@ int main(void)
 
 	for (num = 0; num <= 98; num++)
 	{
-		for (num2 = 1; num2 <= 99; num2++)
+		for (num2 =num+1; num2 <= 99; num2++)
 		{
 			putchar((num / 10) + '0');
 			putchar((num % 10) + '0');
 			putchar(' ');
 			putchar((num2 / 10) + '0');
 			putchar((num2 / 10) + '0');
-			if (num < 98)
-			{
-				putchar(',');
-				putchar(' ');
-			}
+			if (num == 98 && num2 == 99)
+				continue;
+
+			putchar(',');
+			putchar(' ');
 		}
 	}
 
