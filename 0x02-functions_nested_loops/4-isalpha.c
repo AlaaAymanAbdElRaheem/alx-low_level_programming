e4f95b98c4974948d64e0aa58209ae726f7faff9
AlaaAymanAbdElRaheem <alaa251996@gmail.com>
2023-03-22 06:26:43 +0200

    task 4

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,14 +1,14 @@
 #include "main.h"
 
 /**
- * _islower - function checks wither the letter is lowe or not
+ * _isalpha - function chicks alphabets
  * @c: The character to be checked
  * Return: 1 or 0
  */
 
-int _islower(int c)
+int _isalpha(int c)
 {
-	if (c >= 'a' && c <= 'z')
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
 		return (1);
 	else
 		return (0);
