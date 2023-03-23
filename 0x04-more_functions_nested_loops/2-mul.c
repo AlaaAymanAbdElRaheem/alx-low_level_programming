e1ab40125f146292530db26833014809609b2b1c
AlaaAymanAbdElRaheem <alaa251996@gmail.com>
2023-03-23 14:04:15 +0200

    fixing task 2

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,11 +1,10 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
  * mul - multiplies two numbers
  * @a: first number
  * @b: second number
- * Return: 0
+ * Return: the result
  */
 
 int mul(int a, int b)
@@ -13,6 +12,5 @@ int mul(int a, int b)
 	int multi;
 
 	multi = a * b;
-	printf("%d", multi);
-	return (0);
+	return (multi);
 }
