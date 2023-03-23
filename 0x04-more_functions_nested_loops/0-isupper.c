c84a7b7ddfb8249eb63995f228c783d47ebbfc83
AlaaAymanAbdElRaheem <alaa251996@gmail.com>
2023-03-23 13:29:41 +0200

    fixing betty style task 0

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -2,14 +2,14 @@
 
 /**
  * _isupper - checks if the char capital or not
- * @c: the char 
+ * @c: the char
  * Return: 1 or 0
  */
 
 int _isupper(int c)
 {
 	if (c >= 'A' && c <= 'Z')
-		return(1);
+		return (1);
 	else
-		return(0);
+		return (0);
 }
