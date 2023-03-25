0f8710940ad8fa724836d34ab6f1cb977337d576
AlaaAymanAbdElRaheem <alaa251996@gmail.com>
2023-03-26 00:43:29 +0200

    fixing task 9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -11,6 +11,9 @@ void times_table(void)
 
 	for (num = 0; num <= 9; num++)
 	{
+		_putchar('0');
+		_putchar(',');
+		_putchar(' ');
 		for (mult = 1; mult <= 9; mult++)
 		{
 			result = (num * mult);
