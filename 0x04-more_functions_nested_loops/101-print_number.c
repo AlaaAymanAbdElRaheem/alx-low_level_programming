cfeed737f24ca7b4a3bdb9f7a2e971a995e824a9
AlaaAymanAbdElRaheem <alaa251996@gmail.com>
2023-03-25 04:43:16 +0200

    task 12

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -8,5 +8,14 @@
 
 void print_number(int n)
 {
-  
+	unsigned int num = n;
+
+	if (n < 0)
+	{
+		num = n * -1;
+		_putchar('-');
+	}
+	if (num / 10 > 0)
+		print_number(num / 10);
+	_putchar((num % 10) +'0');
 }
