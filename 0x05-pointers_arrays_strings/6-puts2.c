7a4dc7f64c0649f77940af270bf917e4b1f38509
AlaaAymanAbdElRaheem <alaa251996@gmail.com>
2023-03-29 00:00:52 +0200

    fixing task 6

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -9,8 +9,12 @@
 void puts2(char *str)
 {
 	int i;
+	int count = 0;
 
-	for (i = 0; str[i] != '\0'; i += 2)
+	for (i = 0; str[i] != '\0'; i++)
+		count++;
+
+	for (i = 0; i < count; i += 2)
 		_putchar(str[i]);
 
 	_putchar('\n');
