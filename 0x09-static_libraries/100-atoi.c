e321c98a2413e3e44d62999d9211db96d947d141
AlaaAymanAbdElRaheem <alaa251996@gmail.com>
2023-04-08 04:14:04 +0200

    task 0

---
+++ 0x09-static_libraries/100-atoi.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * _atoi - convert string to integers
+ * @s: the string
+ * Return: the converted integer
+ */
+
+int _atoi(char *s)
+{
+	int sign = 1;
+	int i, digit;
+	unsigned int result = 0;
+
+	for (i = 0 ; s[i] != '\0'; i++)
+	{
+		if (s[i] == '-')
+			sign *= -1;
+		else if (s[i] >= '0' && s[i] <= '9')
+		{
+			digit = s[i] - '0';
+			result = result * 10 + digit;
+		}
+		else if (result != 0)
+			break;
+	}
+	return (result * sign);
+}
