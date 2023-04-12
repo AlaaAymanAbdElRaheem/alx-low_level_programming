cdf49b9367a0ce0de540f34fce9914729ddefc50
AlaaAymanAbdElRaheem <alaa251996@gmail.com>
2023-04-12 02:22:17 +0200

    task 1

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -16,7 +16,7 @@ char *_strdup(char *str)
 		return (NULL);
 	for (i = 0; str[i] != '\0'; i++)
 		len++;
-	dup = (char *) malloc(i * sizeof(char));
+	dup = (char *) malloc(len * sizeof(char));
 	if (dup == NULL)
 		return (NULL);
 	for (ii = 0; ii < len; ii++)
