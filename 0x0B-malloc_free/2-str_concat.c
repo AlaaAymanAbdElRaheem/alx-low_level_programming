c6506c261da47d682e0d9134a7706d238e31c387
AlaaAymanAbdElRaheem <alaa251996@gmail.com>
2023-04-12 02:55:46 +0200

    fixing task 2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -13,6 +13,10 @@ char *str_concat(char *s1, char *s2)
 	int i, ii, j = 0, size = 0, len = 0, len2 = 0;
 	char *con;
 
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
 	for (i = 0; s1[i] != '\0'; i++)
 		len++;
 	for (i = 0; s2[i] != '\0'; i++)
