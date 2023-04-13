7a10e59a169f012db4e0499e55355c16465bd408
AlaaAymanAbdElRaheem <alaa251996@gmail.com>
2023-04-13 22:20:18 +0200

    fixing task 4

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -27,7 +27,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	new_ptr = malloc(new_size);
 	if (new_ptr == NULL)
 		return (NULL);
-	for (i = 0; i < old_size || i < new_size; i++)
+	for (i = 0; i < old_size && i < new_size; i++)
 		new_ptr[i] = cptr[i];
 	free(ptr);
 	return (new_ptr);
