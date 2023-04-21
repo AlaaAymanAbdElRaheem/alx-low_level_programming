8b04d643963a3fb7eda2a55a80c8ab7f5b6f72e2
AlaaAymanAbdElRaheem <alaa251996@gmail.com>
2023-04-21 21:41:07 +0200

    task 3

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,8 +1,27 @@
 #ifndef VARIADIC_FUNCTION_H
 #define VARIADIC_FUNCTION_H
 
+#include <stdarg.h>
+
+/**
+ * struct print_type - sturcture
+ * @t: pointer to string of the type
+ * @print_t: pointer to a function
+ */
+
+struct print_type
+{
+char *t;
+void (*print_t)(va_list);
+};
+
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
+void print_int(va_list list);
+void print_float(va_list list);
+void print_char(va_list list);
+void print_string(va_list list);
 
 #endif
