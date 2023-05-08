2625e44deb7fb59489987ffd6aec3ccf8f1cdb79
AlaaAymanAbdElRaheem <alaa251996@gmail.com>
2023-05-08 22:35:57 +0300

    task 2

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -11,10 +11,13 @@ int append_text_to_file(const char *filename, char *text_content)
 {
 	int fd, w, len = 0;
 
-	if (filename == NULL || text_content == NULL)
+	if (filename == NULL)
 		return (-1);
 
-	fd = open(filename, O_APPEND);
+	if (text_content == NULL)
+		return (1);
+
+	fd = open(filename, O_WRONLY | O_APPEND);
 	if (fd == -1)
 		return (-1);
 
