266d6a4a017777105b09e428bb2b18dcc23625e3
AlaaAymanAbdElRaheem <alaa251996@gmail.com>
2023-06-20 06:01:28 +0300

    fixing task 3

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P .. https://https://raw.githubusercontent.com/AlaaAymanAbdElRaheem/alx-low_level_programming/blob/main/0x18-dynamic_libraries/libgigagm.so
+wget -P .. https://raw.githubusercontent.com/AlaaAymanAbdElRaheem/alx-low_level_programming/blob/main/0x18-dynamic_libraries/libgigagm.so
 export LD_PRELOAD=/home/abd/alx/alx-low_level_programming/0x18-dynamic_libraries/libgigagm.so
